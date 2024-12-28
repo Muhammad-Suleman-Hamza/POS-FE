1c38e5314fd5652f3ff631c6e858223184c6ea8b
Muhammad-Suleman-Hamza <sulemanhamzatahir@gmail.com>
2024-12-28 17:31:53 +0500

    reports

--- src/pages/reports/index.jsx
+++ src/pages/reports/index.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
 import { tokens } from "../../theme";
 import Grid from '@mui/material/Grid2';
 import { useSelector } from "react-redux";
 import 'react-date-range/dist/styles.css';
 import { format, addDays } from 'date-fns';
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import StatBox from "../../components/StatBox";
 import 'react-date-range/dist/theme/default.css';
 import { DateRangePicker } from 'react-date-range';
+import { Box, Button, useTheme } from "@mui/material";
 import { datePickerButton } from "../../constants/FormFields";
-import { Box, Button, Typography, useTheme } from "@mui/material";
 
 const Reports = () => {
     const theme = useTheme();
@@ -20,56 +20,86 @@ const Reports = () => {
     const { vendors } = useSelector((state) => state.vendor);
     const { customers } = useSelector((state) => state.customer);
 
+    const [showItems, setShowItems] = useState(items);
+    const [showOrders, setShowOrders] = useState(orders);
+    const [showVendors, setShowVendors] = useState(vendors);
+    const [showCustomers, setShowCustomers] = useState(customers);
+
     const [showPicker, setShowPicker] = useState(false);
     const [date, setDate] = useState([
         {
             key: 'selection',
             startDate: new Date(),
-            endDate: addDays(new Date(), 0)
+            endDate: addDays(new Date(), 1)
         }
     ]);
 
     const stats = [
         {
-            value: items?.length,
+            value: showItems?.length,
             heading: "Total Items",
         },
         {
-            value: orders?.length,
+            value: showOrders?.length,
             heading: "Total Orders",
         },
         {
-            value: vendors?.length,
+            value: showVendors?.length,
             heading: "Total Vendors",
         },
         {
-            value: customers?.length,
+            value: showCustomers?.length,
             heading: "Total Customers",
         }
     ]
 
-    const datePickerHandler = () => {
+    const handleDatePicker = () => {
         setShowPicker(!showPicker);
-        if (showPicker) queryOnDates();
+        if (showPicker) getReports();
     }
 
-    const queryOnDates = () => {
-        console.log('pos :: start date :: ', date[0].startDate);
-        console.log('pos :: end date :: ', date[0].endDate);
+    const getReports = () => {
+        let tempItems = items;
+        let tempOrders = orders;
+        let tempVendors = vendors;
+        let tempCustomers = customers;
+
+        if (!date[0].startDate) {
+            tempItems = items.filter((item) => dateChecker(item.createdDate));
+            tempOrders = orders.filter((order) => dateChecker(order.createdDate));
+            tempVendors = vendors.filter((vendor) => dateChecker(vendor.createdDate));
+            tempCustomers = customers.filter((customer) => dateChecker(customer.createdDate));
+        }
+        
+        setShowItems(tempItems);
+        setShowOrders(tempOrders);
+        setShowVendors(tempVendors);
+        setShowCustomers(tempCustomers);
     }
 
-    const getReportsRage = () => {
-        const end = format(date[0].endDate, 'yyyy-MM-dd');
-        const start = format(date[0].startDate, 'yyyy-MM-dd');
+    const getReportsDateRange = () => {
+        const end = format(date[0].endDate, 'dd-MM-yyyy');
+        const start = format(date[0].startDate, 'dd-MM-yyyy');
 
         return <><b>From:</b> {start} - <b>To:</b> {end}</>;
     }
 
+    const dateChecker = (dateToBeChecked) => {
+        const end = format(date[0].endDate, 'dd-MM-yyyy');
+        const start = format(date[0].startDate, 'dd-MM-yyyy');
+
+        return format(dateToBeChecked, 'dd-MM-yyyy') >= start && format(dateToBeChecked, 'dd-MM-yyyy') <= end;
+    }
+
+    useEffect(() => {
+        getReports();
+    }, [])
+
     return (
         <Box m="20px">
             <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Header title="Reports:" subtitle={getReportsRage()} parentClass="datePickerHeaderStyling"/>
-                <Button {...datePickerButton} onClick={datePickerHandler}>
+                <Header title="Reports:" subtitle={getReportsDateRange()} parentClass="datePickerHeaderStyling" />
+                <Button {...datePickerButton} onClick={handleDatePicker}>
                     {showPicker ? "Close" : "Pick Date"}
                 </Button>
                 {
@@ -81,7 +111,7 @@ const Reports = () => {
                         className="date-picker"
                         showSelectionPreview={true}
                         moveRangeOnFirstSelection={false}
-                        onChange={item => setDate([item.selection])}
+                        onChange={(item) => setDate([item.selection])}
                     />
                 }
             </Box>
