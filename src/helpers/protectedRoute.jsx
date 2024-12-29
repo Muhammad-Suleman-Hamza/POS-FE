13d906027c2305ff5486e11fbfbb8d0571ca4c35
Muhammad-Suleman-Hamza <sulemanhamzatahir@gmail.com>
2024-12-30 01:59:18 +0500

    wipe data on logout and after token expire

--- src/helpers/protectedRoute.jsx
+++ src/helpers/protectedRoute.jsx
@@ -2,10 +2,12 @@ import Loader from '../components/Loader';
 import { useAuth } from '../hooks/useAuth';
 import { Navigate } from 'react-router-dom';
 import { getSessionStorage } from './storage';
+import { useLogout } from '../hooks/useLogout';
 import React, { useEffect, useState } from 'react';
 
 const ProtectedRoute = ({ children }) => {
   const { checkSession } = useAuth();
+  const { emptyEveryThing } = useLogout();
   const [isAuthenticated, setIsAuthenticated] = useState(null); // Null for loading state.
 
   useEffect(() => {
@@ -14,6 +16,7 @@ const ProtectedRoute = ({ children }) => {
         const token = getSessionStorage('sessionToken');
         
         if (!token) {
+          emptyEveryThing();
           setIsAuthenticated(false); // No token, not authenticated.
           return;
         }
@@ -21,8 +24,9 @@ const ProtectedRoute = ({ children }) => {
         const isValid = await checkSession(token);
         setIsAuthenticated(isValid);
       } catch (error) {
-        console.error('Error verifying session:', error);
+        emptyEveryThing();
         setIsAuthenticated(false); // Treat as unauthenticated on errors.
+        console.error('Error verifying session:', error);
       }
     };
 
