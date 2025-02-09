b3e13b92de5d4383c23954911a5f4631969cbe3b
Muhammad-Suleman-Hamza <43912964+Muhammad-Suleman-Hamza@users.noreply.github.com>
2025-02-09 18:57:13 +0500

    Improvements (#30)
    * redirects added
    
    * updated directory
    
    * netlify.toml updated
    
    * removed toml
    
    * added homepage in package.json
    
    * redirects updated
    
    * remove homepage from package.json
    
    * updated base in vite config
    
    * ErrorBoundary
    
    * added logrocket + added sentry
    
    * commented unstable_rowSpanning
    
    * moved header button location +
    remove stats from dashboard +
    only show today orders on dashboard +
    add link to rows of today order +
    add total bill column in today order table
    
    * handle < 0 input for number fields - done
    remove extra conditions from add order page - done
    decrease columns width of order table - done
    move buttons to bottom of heading - done
    remove 0 from dashboard - done
    
    * changed theme to light - done
    decrease sidebar width - done
    
    * search added + order view button moved to left
    
    * add received and pending amount values in customer table as a ledger array when ordering
    
    * added createdDate in customerLedger
    
    * removed extra imports
    
    ---------
    
    Co-authored-by: Suleman Hamza <suleman.hamza@attendanywhere.com>

--- src/pages/order/addOrEdit.jsx
+++ src/pages/order/addOrEdit.jsx
@@ -7,31 +7,25 @@ import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import BasicModal from "../../components/Modal";
+import { getItems } from "../../store/slices/item";
 import { useDispatch, useSelector } from "react-redux";
 import { paymentMethods } from "../../constants/generic";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { addOrder, updateOrder } from "../../store/slices/order";
 import { toggleCreateOrUpdateModal, toggleLoading } from "../../store/slices/common";
 import {
-    Box,
-    useTheme,
-    MenuItem,
-    TextField,
-    Autocomplete
-} from "@mui/material";
-import {
-    Box,
-    useTheme,
-    MenuItem,
-    TextField,
-    Autocomplete
-} from "@mui/material";
-import {
     getLocalStorage,
     setSessionStorage,
     getSessionStorage,
     removeSessionStorage,
 } from "../../helpers/storage";
+import {
+    Box,
+    useTheme,
+    MenuItem,
+    TextField,
+    Autocomplete
+} from "@mui/material";
 import {
     addButton,
     backButton,
@@ -42,7 +36,6 @@ import {
     initialValuesOfCustomer,
     checkoutSchemaOfCustomer,
 } from '../../constants/FormFields'
-import { getItems } from "../../store/slices/item";
 
 const AddOrder = () => {
     const theme = useTheme();
